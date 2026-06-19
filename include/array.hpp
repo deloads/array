eb11aadc4629408524aad1e7067fae548b55db62
Marijn Klappe <mf.klappe@gmail.com>
2026-06-19 12:55:56 +0200

    .

--- include/array.hpp
+++ include/array.hpp
@@ -1,6 +1,53 @@
 #pragma once
 
-struct array
+template <typename Key, typename Value>
+class Array
 {
-    
+private:
+    int size = 0;
+    int capacity = 2;
+
+    Key* keys = nullptr;
+    Value* values = nullptr;
+
+    void resize(int newCapacity){
+        Key* newKeys = new Key[newCapacity];
+        Value* newValues = new Value[newCapacity];
+
+        for(int i = 0; i < size; i++){
+            newKeys[i] = keys[i];
+            newValues[i] = values[i];
+        }
+
+        delete[] keys;
+        delete[] values;
+
+        keys = newKeys;
+        values = newValues;
+
+        capacity = newCapacity;
+    }
+
+    void checkSize(){
+        if(size == capacity){
+            resize(capacity*2);
+        }
+    }
+
+public:
+    Array(){
+        keys = new Key[capacity];
+        values = new Value[capacity];
+    }
+
+    ~Array(){
+        delete[] keys;
+        delete[] values;
+    }
+
+    void set(const Key& key, const Value& value){
+        checkSize();
+
+        
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "array.hpp"
 
+
+struct Vec
+{
+    int x,y;
+
+    Vec() = default;
+    Vec(int x, int y): x(x), y(y) {}
+};
+
+
 int main(){
+
+    Array<Vec,int> a;
+
     return 0;
 }
